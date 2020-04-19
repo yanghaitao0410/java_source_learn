8b77b8b7b9a8be46a02ae8f30dc72ee1efab60f1
yanght <1454385822@qq.com>
2020-04-19 22:29:45 +0800

    init

---
+++ src/cn/water/test/hashmap/Test.java
@@ -0,0 +1,20 @@
+package cn.water.test.hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author yht
+ * @create 2020/4/19
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        HashMap<String, Double> hashMap = new HashMap<>();
+        hashMap.put("01", 0.1);
+        hashMap.put("02", 0.2);
+        hashMap.put("03", 0.3);
+        hashMap.put("04", 0.4);
+        hashMap.put("05", 0.5);
+    }
+
+}
